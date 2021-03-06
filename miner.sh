19e797483790a379637b973d01a0eaaabfec334e
tanaka-tarounosuke-miner <80168385+tanaka-tarounosuke-miner@users.noreply.github.com>
2021-03-07 01:52:41 +0900

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -1,7 +1,7 @@
 docker run -dit \
-    --restart=always \
-    --name miner \
-    --runtime=nvidia \
-    -e NVIDIA_VISIBLE_DEVICES=all \
-    polarbear08/ethminer:nvidia-cuda \
-        -P stratum2+tcp://YOUR_WALLET_ADDRESS.WORKER@daggerhashimoto.eu-west.nicehash.com:3353
+--restart=always \
+--name miner \
+--runtime=nvidia \
+-e NVIDIA_VISIBLE_DEVICES=all \
+polarbear08/ethminer:nvidia-cuda \
+-P stratum2+tcp://33ps7C4eveLZzJrkAK8VLr5Qb9hNyHYyF6.WORKER@daggerhashimoto.eu-west.nicehash.com:3353
